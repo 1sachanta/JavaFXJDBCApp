1d24df27a7306a845e157faa7514fba48ec6baf7
suhas_achanta@yahoo.com <suhas_achanta@yahoo.com>
2020-04-02 09:49:37 -0400

    First Commit

---
+++ src/main/java/com/suhas/jdbc/sample/DAO.java
@@ -0,0 +1,10 @@
+package com.suhas.jdbc.sample;
+
+public interface DAO {
+	   public static final String DB_URL =
+	      "jdbc:mysql://testdb.c87oictu3b1l.us-east-1.rds.amazonaws.com:3306/" +"testdb?zeroDateTimeBehavior=convertToNull";
+	   public static final String DRIVER =
+	      "com.mysql.jdbc.Driver";
+	   public static final String USER = "admin";
+	   public static final String PASS = "Password1";
+	}
